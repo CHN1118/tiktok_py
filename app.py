873994fd67b4824b603ada84ebc493451f1c7fdd
CHN1118 <chn001118@gmail.com>
2025-03-20 18:36:39 +0800

    Changes

--- app.py
+++ app.py
@@ -7,39 +7,45 @@ from util.select_router import list_routes
 
 app = Flask(__name__)
 
-devices_id = 'QKYPIZA6R8IJ7TLV'
+devices_id = "QKYPIZA6R8IJ7TLV"
 # 连接到安卓设备
 d = u2.connect(devices_id)  # 默认连接到第一个设备，如果有多个设备可以指定设备ID
 
-tiktok_bot = tiktok(app,d)
+tiktok_bot = tiktok(app, d)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 # 你可以在这里添加一个路由来触发手机的操作
-@app.route('/tiktok')
+@app.route("/tiktok")
 def tiktok_main():
     """获取所有方法"""
     return list_routes(app)
 
-@app.route('/back_swipe')
+
+@app.route("/back_swipe")
 def back_swipe():
     """模拟系统退出"""
     tiktok_bot.back_swipe()
     return jsonify({"message": "模拟系统退出", "status": "success"})
 
-@app.route('/restart')
+
+@app.route("/restart")
 def restart_tiktok():
     """单独提供一个接口来重启 TikTok"""
     tiktok_bot.restart_app()
     return jsonify({"message": "TikTok 已重启", "status": "success"})
 
-@app.route('/judgment_page')
+
+@app.route("/judgment_page")
 def judgment_page():
     """判断当前页面 TikTok"""
-    s,m = tiktok_bot.judgment_page()
+    s, m = tiktok_bot.judgment_page()
     return jsonify({"message": m, "status": "success"})
+
 
 @app.route("/search_input", methods=["POST"])
 def search_input():
@@ -53,5 +59,6 @@ def search_input():
     s, m = tiktok_bot.search_input(keyword)  # 传入 keyword
     return jsonify({"message": m, "status": "success"})
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)
--- tiktok/controls.py
+++ tiktok/controls.py
@@ -13,10 +13,19 @@ class TikTokAutomation:
         self.d = device  # 存储设备对象
         self.package_name = "com.zhiliaoapp.musically"  # TikTok 包名
         self.stop_flag = False  # 用于控制自动观看的标志
-        self.status_bar_h = device(resourceId="com.android.systemui:id/status_bar").info['bounds']['bottom']
-    
-    #* 抽离公共方法
-    def _click_button(self, class_name: str, description: str = None, descriptionMatches: str = None, text: str = None, index: int = None):
+        self.status_bar_h = device(
+            resourceId="com.android.systemui:id/status_bar"
+        ).info["bounds"]["bottom"]
+
+    # * 抽离公共方法
+    def _click_button(
+        self,
+        class_name: str,
+        description: str = None,
+        descriptionMatches: str = None,
+        text: str = None,
+        index: int = None,
+    ):
         """
         通用按钮点击方法
         :param class_name: 按钮的 class 名称
@@ -26,20 +35,30 @@ class TikTokAutomation:
         """
         try:
             if description:
-                button = self.d(className=class_name, index = index , description=description)
+                button = self.d(
+                    className=class_name, index=index, description=description
+                )
             elif descriptionMatches:
-                button = self.d(className=class_name, index = index , descriptionMatches=descriptionMatches)
+                button = self.d(
+                    className=class_name,
+                    index=index,
+                    descriptionMatches=descriptionMatches,
+                )
             elif text:
-                button = self.d(className=class_name, index = index , text=text)
+                button = self.d(className=class_name, index=index, text=text)
             else:
                 raise ValueError("必须提供description或description_matches")
-            
+
             if button.exists:
                 button.click()
-                logger.success(f"成功点击 {description or descriptionMatches or text} 按钮")
+                logger.success(
+                    f"成功点击 {description or descriptionMatches or text} 按钮"
+                )
                 return True, f"点击{description or descriptionMatches or text}按钮成功"
             else:
-                logger.success(f"未找到 {description or descriptionMatches or text} 按钮")
+                logger.success(
+                    f"未找到 {description or descriptionMatches or text} 按钮"
+                )
                 return False, f"未找到{description or descriptionMatches or text}按钮"
         except Exception as e:
             logger.warning(f"按钮点击失败: {e}")
@@ -76,10 +95,10 @@ class TikTokAutomation:
             width, height = self.d.window_size()
 
             # 计算起点和终点坐标
-            start_x = 0               # 屏幕最左侧
-            start_y = height // 2      # 屏幕垂直方向的中间
-            end_x = width // 2        # 屏幕水平方向的中间
-            end_y = height // 2        # 仍然在中间高度
+            start_x = 0  # 屏幕最左侧
+            start_y = height // 2  # 屏幕垂直方向的中间
+            end_x = width // 2  # 屏幕水平方向的中间
+            end_y = height // 2  # 仍然在中间高度
 
             # 执行滑动
             self.d.swipe(start_x, start_y, end_x, end_y, duration=0.2)  # 0.2秒快速滑动
@@ -155,11 +174,11 @@ class TikTokAutomation:
                 if self.stop_flag:  # 发现 stop_flag 被设置，停止观看
                     logger.success("检测到停止信号，终止自动观看")
                     return False, "自动观看已停止"
-                
+
                 logger.success(f"正在观看第 {i+1} 个视频")
                 time.sleep(delay)  # 模拟观看
                 self.swipe_up()  # 滑动到下一个视频
-                
+
             return True, f"成功观看了 {count} 个视频"
         except Exception as e:
             logger.warning(f"自动观看失败: {e}")
@@ -171,9 +190,27 @@ class TikTokAutomation:
         try:
             # 定义多个搜索条件（每个条件的值会带上标题）
             search_conditions = [
-                {"title": "搜索页面", "condition": {"resourceId": "com.zhiliaoapp.musically:id/fho", "index": 2}},  # 第一个条件
-                {"title": "首页", "condition": {"className": "android.widget.LinearLayout","description":"推荐"}},  # 第二个条件（备用）
-                {"title": "搜索框", "condition": {"text": "搜索", "className": "android.widget.TextView"}},  # 第三个条件（备用）
+                {
+                    "title": "搜索页面",
+                    "condition": {
+                        "resourceId": "com.zhiliaoapp.musically:id/fho",
+                        "index": 2,
+                    },
+                },  # 第一个条件
+                {
+                    "title": "首页",
+                    "condition": {
+                        "className": "android.widget.LinearLayout",
+                        "description": "推荐",
+                    },
+                },  # 第二个条件（备用）
+                {
+                    "title": "搜索框",
+                    "condition": {
+                        "text": "搜索",
+                        "className": "android.widget.TextView",
+                    },
+                },  # 第三个条件（备用）
                 # 可以继续添加其他条件和标题
             ]
 
@@ -192,36 +229,46 @@ class TikTokAutomation:
             logger.warning(f"无法判断: {e}")
             return False, f"无法判断: {e}"
 
-#*  视频页面操作
+    # *  视频页面操作
     def like_video(self):
         """点赞视频"""
         return self._click_button("android.widget.ImageView", description="赞")
 
     def follow_user(self):
         """关注用户"""
-        return self._click_button("android.widget.Button", descriptionMatches="^关注 .*",index=1)
+        return self._click_button(
+            "android.widget.Button", descriptionMatches="^关注 .*", index=1
+        )
 
     def favorite_video(self):
         """收藏视频"""
-        return self._click_button("android.widget.Button", description="将此视频添加到或移出收藏。")
+        return self._click_button(
+            "android.widget.Button", description="将此视频添加到或移出收藏。"
+        )
 
     def share_video(self):
         """分享视频"""
-        return self._click_button("android.widget.Button", descriptionMatches="^分享视频。.*")
+        return self._click_button(
+            "android.widget.Button", descriptionMatches="^分享视频。.*"
+        )
 
     def blogger_home(self):
         """观看视频时——点击头像进入博主主页"""
-        return self._click_button("android.widget.ImageView", descriptionMatches="^.*主页")
+        return self._click_button(
+            "android.widget.ImageView", descriptionMatches="^.*主页"
+        )
 
     def blogger_fallow(self):
         """博主页面关注"""
-        return self._click_button("android.widget.TextView", text="关注",index=0)
-        
-    #* 博主页面第一个视频
+        return self._click_button("android.widget.TextView", text="关注", index=0)
+
+    # * 博主页面第一个视频
     def blogger_first_video(self):
         """博主第一个视频"""
         try:
-            button = self.d(className="android.widget.GridView").child(className="android.widget.FrameLayout")
+            button = self.d(className="android.widget.GridView").child(
+                className="android.widget.FrameLayout"
+            )
             if button.exists:
                 button.click()
                 logger.success("已打开博主第一个视频")
@@ -232,16 +279,15 @@ class TikTokAutomation:
         except Exception as e:
             logger.warning(f"打开失败: {e}")
             return False, f"打开失败: {e}"
-        
-    #* 主页右上角的搜索
+
+    # * 主页右上角的搜索
     def search_video(self):
         """主页右上角的搜索"""
         try:
-            button = self.d(className="android.widget.LinearLayout",description="推荐")
+            button = self.d(className="android.widget.LinearLayout", description="推荐")
             if button.exists:
-                w,h = self.d.window_size()
-                print(f"{w,h,self.status_bar_h}")
-                self.d.click(w - 20,self.status_bar_h + 20)
+                w, h = self.d.window_size()
+                self.d.click(w - 20, self.status_bar_h + 20)
                 logger.success("进入搜索页面")
                 return True, "进入搜索页面"
             else:
@@ -250,9 +296,9 @@ class TikTokAutomation:
         except Exception as e:
             logger.warning(f"进入失败: {e}")
             return False, f"进入失败: {e}"
-        
-    #* 主页点击右上角的搜索
-    def search_inptup(self, keyword: str = ""):
+
+    # * 在搜索框输入关键字并搜索
+    def search_input(self, keyword: str = ""):
         """在搜索框输入关键字并搜索"""
         try:
             # 获取搜索框
@@ -274,7 +320,24 @@ class TikTokAutomation:
             else:
                 logger.warning("未找到搜索框")
                 return False, "未找到搜索框"
-        
+
         except Exception as e:
             logger.warning(f"搜索失败: {e}")
             return False, f"搜索失败: {e}"
+
+    # * 暂停播放
+    def stop_video(self):
+        """暂停播放"""
+        try:
+            button = self.d(className="android.widget.LinearLayout", description="推荐")
+            if button.exists:
+                w, h = self.d.window_size()
+                self.d.click(w / 2, h / 2)
+                logger.success("暂停/播放")
+                return True, "暂停/播放"
+            else:
+                logger.success("未找到按钮")
+                return False, "未找到按钮"
+        except Exception as e:
+            logger.warning(f"进入失败: {e}")
+            return False, f"进入失败: {e}"
